fb478b2cc53bf1bad94d79431d561cd287a02e58
vitorsfernandes-br <vfernandes2@byupathway.edu>
2026-09-05 03:20:20 -0300

    Home Page W01

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
